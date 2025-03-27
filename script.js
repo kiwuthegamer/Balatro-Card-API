bc3e0a2586161d1bd01c5f12c447168176a1f0da
Kevin <kiwuthegamer@gmail.com>
2025-03-27 18:36:02 +0530

    Reverted changes and removed deprecated document.write() call

--- script.js
+++ script.js
@@ -6,32 +6,12 @@ function getUrlParameter(name) {
 
 function randomOption(list) { return list[Math.floor(Math.random() * list.length)] }
 
-// Global decks for Cards and Jokers
-var deckCards = [];
-var deckJokers = [];
-
-// Function to initialize deck for cards
-function initDeckCards() {
-    deckCards = [];
-    for (let i = 1; i <= 52; i++) {
-        deckCards.push(i);
-    }
-}
-
-// Function to initialize deck for jokers
-function initDeckJokers(excludes) {
-    deckJokers = [];
-    for (let i = 1; i <= 155; i++) {
-        if (!excludes.includes(i)) {
-            deckJokers.push(i);
-        }
-    }
-}
-
-// Function to get a random element from a deck and remove it
-function getCardFromDeck(deck) {
-    let idx = Math.floor(Math.random() * deck.length);
-    return deck.splice(idx, 1)[0];
+function random(min, max, exclude = []) {
+    let num;
+    do {
+        num = Math.floor(Math.random() * (max - min + 1) + min);
+    } while (exclude.includes(num));
+    return num;
 }
 
 var jokers = parseInt(getUrlParameter("jokers"));
@@ -45,43 +25,28 @@ var options = []
 if (jokers) { options.push("Jokers/") }
 if (cards) { options.push("Cards/") }
 
-var option = randomOption(options);
+option = randomOption(options);
 imgSRC += option
 
 if (option == "Cards/") {
-    // Choose the card type.
-    var cardTypes = ["BASIC", "BONUS", "GLASS", "GOLD", "LUCKY", "MULT", "STEEL", "WILD"];
-    var cardType = "BASIC";
+    options = ["BASIC", "BONUS", "GLASS", "GOLD", "LUCKY", "MULT", "STEEL", "WILD"];
+    
+    option = "BASIC";
     if (!basicOnly) {
-        cardType = randomOption(cardTypes);
+        option = randomOption(options);
     }
-    imgSRC += cardType;
-    
-    // Initialize deckCards if empty.
-    if (deckCards.length === 0) {
-        initDeckCards();
-    }
-    
-    // Get a random card from deckCards.
-    var randomIndex = getCardFromDeck(deckCards);
+    imgSRC += option;
+
+    var randomIndex = random(1, 52);
     imgSRC += `/${randomIndex}.png`;
 }
 
 if (option == "Jokers/") {
-    let excludes;
     if (specialCases) {
-        excludes = [84, 85, 86, 87, 88, 91, 92, 93, 94, 95];
+        var randomIndex = random(1, 155, exclude=[84, 85, 86, 87, 88, 91, 92, 93, 94, 95]);
     } else {
-        excludes = [8, 25, 28, 32, 36, 42, 70, 72, 74, 78, 84, 85, 86, 87, 88, 91, 92, 93, 94, 95, 110, 121, 128];
+        var randomIndex = random(1, 155, exclude=[8, 25, 28, 32, 36, 42, 70, 72, 74, 78, 84, 85, 86, 87, 88, 91, 92, 93, 94, 95, 110, 121, 128]);
     }
-    
-    // Initialize deckJokers if empty.
-    if (deckJokers.length === 0) {
-        initDeckJokers(excludes);
-    }
-    
-    // Get a random joker from deckJokers.
-    var randomIndex = getCardFromDeck(deckJokers);
     imgSRC += `${randomIndex}.png`;
 }
 
